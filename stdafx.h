ae14e4708c1f7b922d13ae4d9ef6012a635d1202
christiaan <christiaan@CHRISTIAAN>
2016-09-22 10:40:42 +0200

    changed to kinect V2 program

--- stdafx.h
+++ stdafx.h
@@ -1,20 +1,6 @@
 //------------------------------------------------------------------------------
 // <copyright file="stdafx.h" company="Microsoft">
-// 	 
-//	 Copyright 2013 Microsoft Corporation 
-// 	 
-//	Licensed under the Apache License, Version 2.0 (the "License"); 
-//	you may not use this file except in compliance with the License.
-//	You may obtain a copy of the License at
-// 	 
-//		 http://www.apache.org/licenses/LICENSE-2.0 
-// 	 
-//	Unless required by applicable law or agreed to in writing, software 
-//	distributed under the License is distributed on an "AS IS" BASIS,
-//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-//	See the License for the specific language governing permissions and 
-//	limitations under the License. 
-// 	 
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
 // </copyright>
 //------------------------------------------------------------------------------
 
@@ -22,7 +8,9 @@
 
 #pragma once
 
+#ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+#endif
 
 // Windows Header Files
 #include <windows.h>
@@ -32,7 +20,10 @@
 // Direct2D Header Files
 #include <d2d1.h>
 
-#pragma comment ( lib, "d2d1.lib" )
+// Kinect Header files
+#include <Kinect.h>
+
+#pragma comment (lib, "d2d1.lib")
 
 #ifdef _UNICODE
 #if defined _M_IX86
@@ -46,9 +37,9 @@
 
 // Safe release for interfaces
 template<class Interface>
-inline void SafeRelease( Interface *& pInterfaceToRelease )
+inline void SafeRelease(Interface *& pInterfaceToRelease)
 {
-    if ( pInterfaceToRelease != NULL )
+    if (pInterfaceToRelease != NULL)
     {
         pInterfaceToRelease->Release();
         pInterfaceToRelease = NULL;
