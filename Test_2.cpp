b338ec1da7b93982f297412b16ffd4083303f2c9
christiaan <christiaan@CHRISTIAAN>
2016-09-27 17:22:39 +0200

    restructured the solution explorer

---
+++ Test_2.cpp
@@ -0,0 +1,20 @@
+#include "Test_2.h"
+
+
+
+CTest_2::CTest_2()
+{
+}
+
+
+CTest_2::~CTest_2()
+{
+}
+
+CTest_3::CTest_3()
+{
+}
+
+CTest_3::~CTest_3()
+{
+}
---
+++ Test_2.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <string.h>
+#include "resource.h"
+class CTest_2
+{
+public:
+	CTest_2();
+	~CTest_2();
+};
+
+class CTest_3
+{
+public:
+	CTest_3();
+	~CTest_3();
+};
