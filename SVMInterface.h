312d7f8d99ccafa3afa40c15168e512300f60224
Dimitri <dimitri.vargemidis@student.kuleuven.be>
2016-10-10 10:55:54 +0200

    Working SVMInterface.

--- SVMInterface.h
+++ SVMInterface.h
@@ -1,18 +1,6 @@
 #pragma once
 
-#include <vector>
-#include "Gesture.h"
 #include "libsvm-3.21\svm.h"
 
-class SVMInterface {
-public:
-	void train(const std::vector<Gesture> &gestures);
-private:
-	void setAllParameters();
-	void setProblem(const int problemSize, double * labels, svm_node ** dataset);
-	double * getLabels(const std::vector<Gesture> &gestures);
-	svm_node ** rescaleData(const std::vector<Gesture> &gestures);
-
-	void train(const int problemSize, const int dimensions, double dataset[], double labels[]);
-	double test(const int dimensions, double testData[]);
-};
+void train(const int problemSize, const int dimensions, double dataset[], double labels[]);
+double test(const int dimensions, double testData[]);
